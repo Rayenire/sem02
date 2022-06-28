aca96e36ac7d3538740272da3146868f23142437
Rayenire <aeristarg@gmail.com>
2022-06-28 13:00:58 +0300

    task03

---
+++ task03/Program.cs
@@ -0,0 +1,8 @@
+﻿int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+if(n1 % n2 == 0){
+    Console.WriteLine("yes");
+}
+else{
+    Console.WriteLine(n1 % n2);
+}
